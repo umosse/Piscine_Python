61fb79e7f87cadefa1816c23e2da20ac2b48097f
umosse <ulysse.mosse@yahoo.fr>
2026-06-15 17:13:04 +0200

    starting python1

--- Python1/ex00/give_bmi.py
+++ Python1/ex00/give_bmi.py
@@ -1,18 +1,30 @@
-import numpy
+import numpy as numpy
 
 
 def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
 	"""
 
 	"""
-	
+	height_array = numpy.array(height)
+	weight_array = numpy.array(weight)
 
+	if height_array.size != weight_array.size:
+		print("Error: Wrong list size")
+		exit(1)
+
+	bmi_array = numpy.array(weight_array / (height_array ** 2))
+
+	return(bmi_array)
 
 
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
 	"""
 
 	"""
+	bmi_array = numpy.array(bmi)
+	bmi_limit = bmi_array < limit
+
+	return(bmi_limit)
 
 
 
