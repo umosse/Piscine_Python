256eeb47be01cf13ff955cf232c2393b58b581b1
umosse <ulysse.mosse@yahoo.fr>
2026-06-15 13:57:49 +0200

    starting python1

---
+++ Python1/ex00/give_bmi.py
@@ -0,0 +1,25 @@
+import numpy
+
+
+def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+	"""
+
+	"""
+	
+
+
+
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+	"""
+
+	"""
+
+
+
+def	main():
+	give_bmi()
+	apply_limit()
+
+
+if __name__ == "__main__":
+	main()
