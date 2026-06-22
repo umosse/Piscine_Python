f5ae0da71e5ed12f55fe2036456926fbb3585755
umosse <ulysse.mosse@yahoo.fr>
2026-06-22 16:14:37 +0200

    ex04

--- Python1/ex04/rotate.py
+++ Python1/ex04/rotate.py
@@ -3,6 +3,23 @@ import cv2
 import numpy as numpy
 
 
+def ft_transpose(img: numpy.ndarray) -> numpy.ndarray:
+	"""
+	
+	"""
+
+	rows = img.shape[0]
+	columns = img.shape[1]
+	channels = img.shape[2]
+
+	transposed = numpy.zeros((columns, rows, channels), dtype=img.dtype)
+
+	for r in range(rows):
+		for c in range(columns):
+			transposed[c][r] = img[r][c]
+	
+	return(transposed)
+
 def ft_rotate(img_array: numpy.ndarray) -> numpy.ndarray:
 	"""
 	
@@ -24,21 +41,23 @@ def ft_rotate(img_array: numpy.ndarray) -> numpy.ndarray:
 
 	z_height, z_width, z_channel = zoomed_img.shape
 	print('The shape of image is:', (z_height, z_width, z_channel))
-	return zoomed_img
+	print(zoomed_img)
 
+	rotated_img = ft_transpose(zoomed_img)
+	r_height, r_width, r_channel = rotated_img.shape
+	print('New shape after transpose:', (r_height, r_width))
 
-loaded_img = ft_load("animal.jpeg")
+	return rotated_img
 
-zoomed = ft_rotate(loaded_img)
-print(zoomed)
-
-cv2.imshow("", zoomed)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
 
 def	main():
 	img_array = ft_load("animal.jpeg")
-	ft_rotate(img_array)
+	rotated = ft_rotate(img_array)
+	print(rotated)
+
+	cv2.imshow("", rotated)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
 
 if __name__ == "__main__":
 	main()
