6ec9badfd89dba11d6bf88b476b7865fab2fe112
umosse <ulysse.mosse@yahoo.fr>
2026-06-29 15:09:48 +0200

    ex00

---
+++ Python2/ex00/load_csv.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+
+def load(path: str) -> pd.DataFrame:
+	"""
+	Takes a path as argument and returns a data set after writing its dimensions.
+	"""
+
+	try:
+		data_set = pd.read_csv(path)
+
+		dimensions = data_set.shape
+		print('Loading dataset of dimensions', dimensions)
+
+		return data_set
+	
+	except (FileNotFoundError, pd.errors.ParserError, pd.errors.EmptyDataError) as err:
+		print("Error loading file at", path, err)
+		return None
+	except Exception as err:
+		print("An unexpected error occured", err)
+		return None
+	
+def	main():
+	data_set = load("")
+	print(data_set)
+
+
+if __name__ == "__main__":
+	main()
